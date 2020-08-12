fb97c845c05a1251d6ff2ce17030df0397f4e4e2
José L. S. A <alagunasalahaddin@live.com>
2020-08-13 00:28:13 +0300

    Creating the initial store.

---
+++ src/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from "redux"
+import productsReducer from "./productsReducer"
+
+export default combineReducers({ products: productsReducer })
---
+++ src/reducers/productsReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  products: [],
+  error: false,
+  loading: false,
+}
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
